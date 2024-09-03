f8114e8efabd7934ed6f05eaf550613072a5557e
stefansiebers <s3f3s-github@yahoo.com>
2024-09-03 11:34:31 +0200

    add another new line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("New line in feature 1");
+        System.out.println("Another new line");
     }
 }
